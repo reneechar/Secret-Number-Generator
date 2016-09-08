c40233d1a93691d244351c10a0f29421bcfbb700
Renee <rchar20@gmail.com>
2016-09-07 17:36:37 -1000

    Returns a secret number and returns a different random number by different generators.

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,8 @@
 'use strict';
 module.exports = function() {
-  
-};
+  let randomNumber = 0;
+  return function (){
+    randomNumber = Math.floor(Math.random()*(100000)+1);
+    return randomNumber;
+  };
+}
